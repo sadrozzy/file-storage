f7a74bff94b578e9184dd4f4338e3ab3a6702f82
so-so <sadrozzycompany@gmail.com>
2024-07-03 22:31:48 +0300

    fix: small fixes

--- client/app/api/http/index.ts
+++ client/app/api/http/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const $api = axios.create({
     withCredentials: true,
-    baseURL: "http://localhost:4000/api/"
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/`
 })
 
 // @ts-ignore
